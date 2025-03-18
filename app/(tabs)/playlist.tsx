c5c325a0beb62547bb228256b20f32113b750ab9
Lalaina0904 <lalainap19.aps2b@gmail.com>
2025-03-18 17:19:29 +0300

    feat: update font for playlist screen

--- app/(tabs)/playlist.tsx
+++ app/(tabs)/playlist.tsx
@@ -5,7 +5,11 @@ export default function Playlist() {
     return (
         <View style={className`bg-[#020024] h-full p-4`}>
             <View className="flex-1 items-center justify-center">
-                <Text style={className`text-white`}>
+                <Text
+                    style={[
+                        className`text-white text-lg`,
+                        { fontFamily: 'Iceland_400Regular' },
+                    ]}>
                     Hello, This is the Playlist screen!
                 </Text>
             </View>
